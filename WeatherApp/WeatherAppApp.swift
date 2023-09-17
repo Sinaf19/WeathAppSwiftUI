0f9e402685d678d6c0eae41ae7f496e16a08f6e5
Sinaf19 <quentinsurdez@gmail.com>
2023-09-17 13:49:40 +0200

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Quentin Surdez on 17.09.23.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
